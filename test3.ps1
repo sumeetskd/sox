bf588dc43ce2a19a90ac46cf4860f7877f80ba84
Sumeet Kumar Das <41441163+sumeetskd@users.noreply.github.com>
2023-09-15 03:11:05 +0530

    excel changes

--- test3.ps1
+++ test3.ps1
@@ -1,41 +1,31 @@
-$xlCellTypeLastCell = 11 
+$xlCellTypeLastCell = 2
 $startRow = 1
-$col = 2
+$startCol = 1
+$endCol = 3
 
 $excel = New-Object -Com Excel.Application
-$wb = $excel.workbooks.open("C:\Users\sumee\Desktop\powershell\sox\new_data.xlsx")
+$wb = $excel.workbooks.open("C:\Users\sumee\Desktop\powershell\sox\excel.xlsx")
 
-# for ($i = 1; $i -le $wb.Sheets.Count; $i++)
-# {
-#     $sh = $wb.Sheets.Item($i)
-#     $endRow = $sh.UsedRange.SpecialCells($xlCellTypeLastCell).Row
-#     $city = $sh.Cells.Item($startRow, $col).Value2
-#     $rangeAddress = $sh.Cells.Item($startRow + 1, $col).Address() + ":" + $sh.Cells.Item($endRow, $col).Address()
-#     $sh.Range($rangeAddress).Value2 | foreach {
-#         New-Object PSObject -Property @{City = $city; Area = $_ }
-#     }
-# }
 
 $sh = $wb.Sheets.Item(1)
 $endRow = $sh.UsedRange.SpecialCells($xlCellTypeLastCell).Row
-$city = $sh.Cells.Item($startRow, $col).Value2
-$rangeAddress = $sh.Cells.Item($startRow, $col).Address() + ":" + $sh.Cells.Item($endRow, $col).Address()
-# $sh.Range($rangeAddress).Value2 | foreach {
-#     New-Object PSObject -Property @{City = $city; Area = $_ }
-# }
+Write-Host "End Row Value - $($endRow)"
+$sheetTitle = "Test"
+$rangeAddress = $sh.Cells.Item($startRow, $startCol).Address() + ":" + $sh.Cells.Item($endRow, $endCol).Address()
+
 $data = $sh.Range($rangeAddress).Value2
 
 $data
 
-# $newSheet = $wb.Sheets.Add()
+$newSheet = $wb.Sheets.Add()
 
-# # Set the sheet name to the city
-# $newSheet.Name = $city
+# Set the sheet name
+$newSheet.Name = $sheetTitle
 
-# # Populate the new worksheet with the data
-# $newSheet.Cells.Item(1, 1).Resize($endRow - $startRow + 1, 1).Value2 = $data
+# Populate the new worksheet with the data
+$newSheet.Cells.Item(1, 1).Resize($endRow - $startRow + 1, $endCol).Value2 = $data
 
-# $newWorkbookPath = "C:\Users\sumee\Desktop\powershell\sox\new_data.xlsx"
-# $wb.SaveAs($newWorkbookPath)
+$newWorkbookPath = "C:\Users\sumee\Desktop\powershell\sox\new_data3.xlsx"
+$wb.SaveAs($newWorkbookPath)
 
 $excel.Workbooks.Close()
